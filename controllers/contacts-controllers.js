2519a46d6a528e7e8122c433073106859b726a0b
Andrey9019 <129278140+Andrey9019@users.noreply.github.com>
2023-12-29 00:48:57 +0200

    add code for task / add models->Contact.js and finish first patch

--- controllers/contacts-controllers.js
+++ controllers/contacts-controllers.js
@@ -1,5 +1,6 @@
 import { HttpError } from "../helpers/index.js";
-import * as contactsServise from "../models/contacts.js";
+
+import Contact from "../models/Contact.js";
 
 import {
   contactAddShema,
@@ -7,78 +8,74 @@ import {
 } from "../shemas/contact-shemas.js";
 
 const listContacts = async (req, res, next) => {
-  try {
-    const result = await contactsServise.listContacts();
+  const result = await Contact.find();
 
-    res.json(result);
-  } catch (error) {
-    next(error);
-  }
+  res.json(result);
 };
 
-const getContactById = async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const result = await contactsServise.getContactById(id);
+// const getContactById = async (req, res, next) => {
+//   try {
+//     const { id } = req.params;
+//     const result = await contactsServise.getContactById(id);
 
-    if (!result) {
-      throw HttpError(404);
-    }
+//     if (!result) {
+//       throw HttpError(404);
+//     }
 
-    res.json(result);
-  } catch (error) {
-    next(error);
-  }
-};
+//     res.json(result);
+//   } catch (error) {
+//     next(error);
+//   }
+// };
 
-const addContact = async (req, res, next) => {
-  try {
-    const { error } = contactAddShema.validate(req.body);
-    if (error) {
-      throw HttpError(400, error.message);
-    }
-    const result = await contactsServise.addContact(req.body);
-    res.status(201).json(result);
-  } catch (error) {
-    next(error);
-  }
-};
+// const addContact = async (req, res, next) => {
+//   try {
+//     const { error } = contactAddShema.validate(req.body);
+//     if (error) {
+//       throw HttpError(400, error.message);
+//     }
+//     const result = await contactsServise.addContact(req.body);
+//     res.status(201).json(result);
+//   } catch (error) {
+//     next(error);
+//   }
+// };
 
-const updateBiId = async (req, res, next) => {
-  try {
-    const { error } = contactUpdateShema.validate(req.body);
-    if (error) {
-      throw HttpError(400, error.message);
-    }
-    const { id } = req.params;
-    const result = await contactsServise.updateContactsById(id, req.body);
-    if (!result) {
-      throw HttpError(404, `Contact with id=${id} not found`);
-    }
-    res.json(result);
-  } catch (error) {
-    next(error);
-  }
-};
+// const updateBiId = async (req, res, next) => {
+//   try {
+//     const { error } = contactUpdateShema.validate(req.body);
+//     if (error) {
+//       throw HttpError(400, error.message);
+//     }
+//     const { id } = req.params;
+//     const result = await contactsServise.updateContactsById(id, req.body);
+//     if (!result) {
+//       throw HttpError(404, `Contact with id=${id} not found`);
+//     }
+//     res.json(result);
+//   } catch (error) {
+//     next(error);
+//   }
+// };
 
-const deleteByID = async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const result = await contactsServise.removeContact(id);
-    if (!result) {
-      throw HttpError(404);
-    }
+// const deleteByID = async (req, res, next) => {
+//   try {
+//     const { id } = req.params;
+//     const result = await contactsServise.removeContact(id);
+//     if (!result) {
+//       throw HttpError(404);
+//     }
 
-    res.json({ message: "Contact deleted" });
-  } catch (error) {
-    next(error);
-  }
-};
+//     res.json({ message: "Contact deleted" });
+//   } catch (error) {
+//     next(error);
+//   }
+// };
 
 export default {
   listContacts,
-  getContactById,
-  addContact,
-  updateBiId,
-  deleteByID,
+  // getContactById,
+  // addContact,
+  // updateBiId,
+  // deleteByID,
 };
