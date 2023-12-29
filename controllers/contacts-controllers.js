7d828d16a2f5c9782236a29d0a3f4f6b27fc9021
Andrey9019 <129278140+Andrey9019@users.noreply.github.com>
2023-12-30 01:35:51 +0200

    add decorators for document

--- controllers/contacts-controllers.js
+++ controllers/contacts-controllers.js
@@ -1,81 +1,57 @@
+import Contact from "../models/Contact.js";
+
 import { HttpError } from "../helpers/index.js";
 
-import Contact from "../models/Contact.js";
+import { ctrlWrapper } from "../decorators/index.js";
 
 import {
   contactAddShema,
   contactUpdateShema,
 } from "../shemas/contact-shemas.js";
 
-const listContacts = async (req, res, next) => {
+const listContacts = async (req, res) => {
   const result = await Contact.find();
 
   res.json(result);
 };
 
-// const getContactById = async (req, res, next) => {
-//   try {
-//     const { id } = req.params;
-//     const result = await contactsServise.getContactById(id);
+// const getContactById = async (req, res, ) => {
+//   const { id } = req.params;
+//   const result = await contactsServise.getContactById(id);
 
-//     if (!result) {
-//       throw HttpError(404);
-//     }
-
-//     res.json(result);
-//   } catch (error) {
-//     next(error);
+//   if (!result) {
+//     throw HttpError(404);
 //   }
+//   res.json(result);
 // };
 
-// const addContact = async (req, res, next) => {
-//   try {
-//     const { error } = contactAddShema.validate(req.body);
-//     if (error) {
-//       throw HttpError(400, error.message);
-//     }
-//     const result = await contactsServise.addContact(req.body);
-//     res.status(201).json(result);
-//   } catch (error) {
-//     next(error);
-//   }
+// const addContact = async (req, res) => {
+//   const result = await contactsServise.addContact(req.body);
+//   res.status(201).json(result);
 // };
 
-// const updateBiId = async (req, res, next) => {
-//   try {
-//     const { error } = contactUpdateShema.validate(req.body);
-//     if (error) {
-//       throw HttpError(400, error.message);
-//     }
-//     const { id } = req.params;
-//     const result = await contactsServise.updateContactsById(id, req.body);
-//     if (!result) {
-//       throw HttpError(404, `Contact with id=${id} not found`);
-//     }
-//     res.json(result);
-//   } catch (error) {
-//     next(error);
+// const updateBiId = async (req, res) => {
+//   const { id } = req.params;
+//   const result = await contactsServise.updateContactsById(id, req.body);
+//   if (!result) {
+//     throw HttpError(404, `Contact with id=${id} not found`);
 //   }
+//   res.json(result);
 // };
 
-// const deleteByID = async (req, res, next) => {
-//   try {
-//     const { id } = req.params;
-//     const result = await contactsServise.removeContact(id);
-//     if (!result) {
-//       throw HttpError(404);
-//     }
-
-//     res.json({ message: "Contact deleted" });
-//   } catch (error) {
-//     next(error);
+// const deleteByID = async (req, res, ) => {
+//   const { id } = req.params;
+//   const result = await contactsServise.removeContact(id);
+//   if (!result) {
+//     throw HttpError(404);
 //   }
+//   res.json({ message: "Contact deleted" });
 // };
 
 export default {
-  listContacts,
-  // getContactById,
-  // addContact,
-  // updateBiId,
-  // deleteByID,
+  listContacts: ctrlWrapper(listContacts),
+  // getContactById : ctrlWrapper(getContactById),
+  // addContact: ctrlWrapper(addContact),
+  // updateBiId: ctrlWrapper(updateBiId),
+  // deleteByID : ctrlWrapper(deleteByID),
 };
--- routes/api/contacts-router.js
+++ routes/api/contacts-router.js
@@ -4,15 +4,32 @@ import contactsControllers from "../../controllers/contacts-controllers.js";
 
 import { isEmptyBody } from "../../middlewares/index.js";
 
+import { validateBody } from "../../decorators/index.js";
+
+import {
+  contactAddShema,
+  contactUpdateShema,
+} from "../../shemas/contact-shemas.js";
+
 const contactsRouter = express.Router();
 
 contactsRouter.get("/", contactsControllers.listContacts);
 
 // contactsRouter.get("/:id", contactsControllers.getContactById);
 
-// contactsRouter.post("/", isEmptyBody, contactsControllers.addContact);
+// contactsRouter.post(
+//   "/",
+//   isEmptyBody,
+//   validateBody(contactAddShema),
+//   contactsControllers.addContact
+// );
 
-// contactsRouter.put("/:id", isEmptyBody, contactsControllers.updateBiId);
+// contactsRouter.put(
+//   "/:id",
+//   isEmptyBody,
+//   validateBody(contactUpdateShema),
+//   contactsControllers.updateBiId
+// );
 
 // contactsRouter.delete("/:id", contactsControllers.deleteByID);
 
