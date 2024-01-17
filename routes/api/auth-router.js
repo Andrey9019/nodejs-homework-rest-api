401209ab345d589f4280880ab4c553e10b55aa4a
Andrey9019 <129278140+Andrey9019@users.noreply.github.com>
2024-01-17 15:34:19 +0200

    add routes avatars

--- routes/api/auth-router.js
+++ routes/api/auth-router.js
@@ -2,7 +2,7 @@ import express from "express";
 
 import { validateBody } from "../../decorators/index.js";
 
-import { isEmptyBody, authenticate } from "../../middlewares/index.js";
+import { isEmptyBody, authenticate, upload } from "../../middlewares/index.js";
 
 import { userRegisterSchema, userLoginSchema } from "../../models/User.js";
 
@@ -27,5 +27,12 @@ authRouter.post(
 authRouter.get("/current", authenticate, authController.getCurrent);
 
 authRouter.post("/logout", authenticate, authController.logout);
+
+authRouter.patch(
+  "/avatars",
+  authenticate,
+  upload.single("avatar"),
+  authController.updateAvatar
+);
 
 export default authRouter;
